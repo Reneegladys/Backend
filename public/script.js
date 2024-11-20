56211ce675c3a38393495fab557dd078ebf9b708
Renée Gladys <renee16@live.se>
2024-11-20 22:19:10 +0100

    learning api

---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.getElementById('playButton').addEventListener('click', function () {
+    // Play music logic goes here
+    console.log('Playing music...');
+  });
+  
+  document.getElementById('pauseButton').addEventListener('click', function () {
+    // Pause music logic goes here
+    console.log('Pausing music...');
+  });
+  
