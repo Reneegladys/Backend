a180c061eff3cffd6f30e17f417bbb4daf9fffe3
Renée Gladys <renee16@live.se>
2024-11-30 19:37:25 +0100

    http request, update, delte done

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -25,6 +25,37 @@ const getEvents = async (req, res) => {
     }
 };
 
+const updateEvent = async (req, res) => {
+    const { id } = req.params;
+    const { name, location, date } = req.body; 
+    try {
+        const updatedEvent = await Event.findByIdAndUpdate(
+            id, 
+            { name, location, date }, 
+            { new: true } 
+        );
+        if (!updatedEvent) {
+            return res.status(404).json({ message: 'Event not found' });
+        }
+        res.status(200).json(updatedEvent);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error'); 
+    }
+};
 
+const deleteEvent = async (req, res) => {
+    const { id } = req.params; 
+    try {
+        const deletedEvent = await Event.findByIdAndDelete(id);
+        if (!deletedEvent) {
+            return res.status(404).json({ message: 'Event not found' });
+        }
+        res.status(200).json({ message: 'Event deleted successfully', event: deletedEvent });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error'); 
+    }
+};
 
-module.exports = { createEvent, getEvents, };
+module.exports = { createEvent, getEvents, updateEvent, deleteEvent };
